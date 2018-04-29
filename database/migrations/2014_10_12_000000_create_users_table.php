d8a788f84f8e5dac07023b3daf1a1413b9955fff
aung-zan <nightkiller10@gmail.com>
2018-04-29 13:35:30 +0630

    #2 created migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,19 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->bigIncrements('id');
+            $table->string('name')                              ->comment('save name of users.');
+            $table->string('email')         ->unique()          ->comment('save email of users.');
+            $table->string('password')                          ->comment('save password of users.');
+            $table->string('phone_number')                      ->comment('save phone number of users.');
+            $table->tinyInteger('premium')  ->default(0)        ->comment('save types of users. (premium or normal)');
+            $table->integer('state')        ->nullable()        ->comment('save state that user living.');
+            $table->string('township')      ->nullable()        ->comment('save township that user living.');
+            $table->rememberToken()                             ->comment('password remember token of user.');
             $table->timestamps();
+            $table->tinyInteger('deleted')  ->default(0)        ->comment('save types of users. (deleted or not)');
+            $table->charset     = 'utf8mb4';
+            $table->collation   = 'utf8mb4_unicode_ci';
         });
     }
 
